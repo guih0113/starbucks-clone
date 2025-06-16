2b926d9e3670ae73b46bd52fa7dcfbc5f200095d
guih0113 <gui.henriquess13@gmail.com>
2025-06-16 17:45:04 -0300

    building header for largest screens

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,3 +1,4 @@
+import { MapPin } from "lucide-react";
 import Logo from "../logo";
 import { SheetMenu } from "./sheetMenu";
 import { Open_Sans } from "next/font/google";
@@ -11,21 +12,51 @@ const openSans = Open_Sans({
 export function Header() {
 	return (
 		<header>
-			<div className="flex justify-between items-center p-4">
-				<div>
-					<Logo />
+			<div className="flex justify-between items-center py-4 px-6 md:py-7 md:px-11">
+				<div className="flex items-center gap-12">
+					<div className="w-10 h-10 md:w-14 md:h-14">
+						<Logo />
+					</div>
+
+					<nav className="hidden md:block">
+						<ul className="flex gap-6 font-bold text-md tracking-widest">
+							<li className="cursor-pointer hover:text-green-700">MENU</li>
+							<li className="cursor-pointer hover:text-green-700">REWARDS</li>
+							<li className="cursor-pointer hover:text-green-700">
+								GIFT CARDS
+							</li>
+						</ul>
+					</nav>
 				</div>
 
-				<div>
+				<div className="hidden md:flex items-center gap-12">
+					<div className="flex items-center gap-3 cursor-pointer hover:text-green-700 text-md font-extrabold">
+						<MapPin />
+						<span className="font-bold">Find a store</span>
+					</div>
+
+					<div className="space-x-4">
+						<button className="border border-black rounded-xl px-5 py-1 font-semibold text-md cursor-pointer hover:bg-gray-200">
+							Sign in
+						</button>
+						<button className="bg-black text-white rounded-xl px-5 py-1.5 font-bold text-md cursor-pointer hover:bg-black/70">
+							Join now
+						</button>
+					</div>
+				</div>
+
+				<div className="flex md:hidden">
 					<SheetMenu />
 				</div>
 			</div>
 
-			<div className="bg-darkgreen text-white px-4 py-3 flex justify-between items-center">
-				<h1 className={`${openSans.variable} font-bold text-sm tracking-wide`}>
+			<div className="bg-darkgreen text-white px-4 py-3 md:py-4 flex justify-between items-center">
+				<h1
+					className={`${openSans.variable} font-extrabold text-sm md:text-md tracking-wide md:tracking-widest md:ml-7`}
+				>
 					STARBUCKS® REWARDS
 				</h1>
-				<button className="border border-white rounded-xl px-4 py-1 font-bold text-sm">
+				<button className="border border-white rounded-xl px-4 py-1 font-bold text-sm flex md:hidden">
 					Join in the app
 				</button>
 			</div>
