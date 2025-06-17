ffdbd137713cb5a315e4a42d3609adc9be185db4
guih0113 <gui.henriquess13@gmail.com>
2025-06-16 22:51:18 -0300

    initializate the construction of the second section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/header/page";
+import { SectionMain } from "@/components/section-main/page";
+import { SectionSecondary } from "@/components/section-secondary/page";
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<SectionMain />
+			<SectionSecondary />
 		</div>
 	);
 }
---
+++ src/components/section-secondary/page.tsx
@@ -0,0 +1,51 @@
+export function SectionSecondary() {
+	return (
+		<section>
+			<div>
+				<div>
+					<h2>Getting started is easy</h2>
+					<p>Earn Stars and get rewarded in a few easy steps.</p>
+				</div>
+
+				<div className="flex flex-col">
+					<div className="flex">
+						<div className="h-16 w-16 border-3 border-green-700 rounded-full text-green-700 flex items-center justify-center text-2xl">
+							1
+						</div>
+						<div>
+							<h3>Create an account</h3>
+							<p>
+								To get started, join now. You can also Join in the app to get
+								access to the full range of Starbucks® Rewards benefits.
+							</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="h-16 w-16 border-3 border-green-700 rounded-full text-green-700 flex items-center justify-center text-2xl">
+							1
+						</div>
+						<div>
+							<h3>Create an account</h3>
+							<p>
+								To get started, join now. You can also Join in the app to get
+								access to the full range of Starbucks® Rewards benefits.
+							</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="h-16 w-16 border-3 border-green-700 rounded-full text-green-700 flex items-center justify-center text-2xl">
+							1
+						</div>
+						<div>
+							<h3>Create an account</h3>
+							<p>
+								To get started, join now. You can also Join in the app to get
+								access to the full range of Starbucks® Rewards benefits.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
