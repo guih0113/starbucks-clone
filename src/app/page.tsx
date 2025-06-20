6eaaf80b3932f8bbd39a1c164cf56168612b48d5
guih0113 <gui.henriquess13@gmail.com>
2025-06-19 22:20:44 -0300

    render sticky container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/header/page"
 import { SectionMain } from "@/components/section-main/page"
 import { SectionSecondary } from "@/components/section-secondary/page"
+import StickyContainer from "@/components/sticky-container/page"
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<StickyContainer />
 			<SectionMain />
 			<SectionSecondary />
 		</div>
