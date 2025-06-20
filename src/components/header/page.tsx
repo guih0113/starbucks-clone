41507bb100cd7d51e0d847f38f83a5e612bbad4c
guih0113 <gui.henriquess13@gmail.com>
2025-06-19 22:22:19 -0300

    remove imports

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,13 +1,6 @@
 import { MapPin } from "lucide-react"
 import Logo from "../logo"
 import { SheetMenu } from "./sheetMenu"
-import { Open_Sans } from "next/font/google"
-
-const openSans = Open_Sans({
-	subsets: ["latin"],
-	weight: ["700"], // bold
-	variable: "--font-open-sans",
-})
 
 export function Header() {
 	return (
@@ -48,17 +41,6 @@ export function Header() {
 				<div className="flex md:hidden">
 					<SheetMenu />
 				</div>
-			</div>
-
-			<div className="bg-darkgreen text-white px-4 py-3 md:py-4 flex justify-between items-center">
-				<h1
-					className={`${openSans.variable} font-black text-sm md:text-md tracking-wide md:tracking-widest md:ml-7`}
-				>
-					STARBUCKS® REWARDS
-				</h1>
-				<button className="border border-white rounded-xl px-4 py-1 font-bold text-sm flex md:hidden">
-					Join in the app
-				</button>
 			</div>
 		</header>
 	)
