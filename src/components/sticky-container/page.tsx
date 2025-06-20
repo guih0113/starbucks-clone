57983913b28e57b30b9b9520f850d3fa9559135d
guih0113 <gui.henriquess13@gmail.com>
2025-06-19 23:18:01 -0300

    fix font size

--- src/components/sticky-container/page.tsx
+++ src/components/sticky-container/page.tsx
@@ -10,7 +10,7 @@ export default function StickyContainer() {
 	return (
 		<div className="sticky top-0 z-10 bg-darkgreen text-white px-4 py-3 md:py-4 flex justify-between items-center">
 			<h1
-				className={`${openSans.variable} font-black text-sm md:text-md tracking-wide md:tracking-widest md:ml-7`}
+				className={`${openSans.variable} font-black text-sm md:text-base tracking-wide md:tracking-widest md:ml-7`}
 			>
 				STARBUCKS® REWARDS
 			</h1>
