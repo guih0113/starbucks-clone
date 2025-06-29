ea7b7833cb88d39c5979f7b400e5c72cc3b2b766
guih0113 <gui.henriquess13@gmail.com>
2025-06-29 09:59:49 -0300

    fix footer size and add details

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -66,7 +66,7 @@ export const dropdowns = [
 
 export function Footer() {
 	return (
-		<footer className="pt-4 pb-28 md:px-14">
+		<footer className="px-4 pt-4 pb-28 md:px-14">
 			<div className="lg:hidden">
 				<FooterWithDropdown />
 			</div>
@@ -76,45 +76,45 @@ export function Footer() {
 			<Separator className="my-4" />
 			<div className="flex flex-col gap-6">
 				<div className="mt-6 flex gap-1">
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiSpotify className="size-7" />
 					</div>
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiFacebook className="size-7" />
 					</div>
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiPinterest className="size-7" />
 					</div>
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiInstagram className="size-7" />
 					</div>
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiYoutube className="size-7" />
 					</div>
-					<div className="flex size-12 items-center justify-center rounded-full hover:bg-gray-200">
+					<div className="flex size-12 cursor-pointer items-center justify-center rounded-full hover:bg-gray-200">
 						<SiX className="size-7" />
 					</div>
 				</div>
-				<div className="flex flex-col gap-4">
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+				<div className="ml-3 flex flex-col gap-4 font-medium">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Privacy Notice
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Consumer Health Privacy Notice
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Terms of Use
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Do Not Share My Personal Information
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						CA Supply Chain Act
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Accessibility
 					</span>
-					<span className="cursor-pointer text-base text-gray-800 hover:underline">
+					<span className="w-fit cursor-pointer text-lg text-gray-800 hover:underline">
 						Cookie Preferences
 					</span>
 					<span className="mt-2 text-sm text-gray-500">
